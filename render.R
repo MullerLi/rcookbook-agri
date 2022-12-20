3e1b65bd634871102e87e7d65f0424cadfbd1bf7
MullerLi <lemax112@gmail.com>
2022-12-20 20:12:15 +0800

    11

---
+++ render.R
@@ -0,0 +1,11 @@
+bookdown::render_book(input ="01-intro.rmd" ,output_format ='bookdown::gitbook',encoding='UTF-8')
+
+bookdown::render_book(input ="02-describe.rmd" ,output_format ='bookdown::gitbook',encoding='UTF-8')
+
+bookdown::render_book(input ="03-ttest.rmd" ,output_format ='bookdown::gitbook',encoding='UTF-8')
+
+bookdown::render_book(input ="04-anova.rmd" ,output_format ='bookdown::gitbook',encoding='UTF-8')
+
+bookdown::render_book(input ="05-GLHG.rmd" ,output_format ='bookdown::gitbook',encoding='UTF-8')
+
+bookdown::render_book(input ="index.rmd" ,output_format ='bookdown::gitbook',encoding='UTF-8')
